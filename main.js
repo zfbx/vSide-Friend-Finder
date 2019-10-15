d6683c7430bca9f88c38ff837b4cb7ddf5680619
Tony <1414927+zfbTony@users.noreply.github.com>
2019-10-15 07:51:01 -0400

    moved css and js outside and added icon

---
+++ main.js
@@ -0,0 +1,403 @@
+var users = [
+    {Name: "Tony", Discord: "142831624868855808", Facebook: "zftonybx", OldNames: [
+        "--Tony--", "Benzin89120", "Pentony8", "OwlCity", "NeruoToxin"
+    ]},
+    {Name: "Kayleigh", Discord: "209761601711112192", Facebook: "xLovelySorrowsx", OldNames: [
+        "Revvenga"
+    ]},
+    {Name: "Alyx", Discord: "355147909425135617", Facebook: "", OldNames: [
+        "--Gabby_Likey--", "phobeastily"
+    ]},
+    {Name: "Rick", Discord: "561422702221721611", Facebook: "", OldNames: [
+        "Vandetta", "iReckless"
+    ]},
+    {Name: "Zay", Discord: "334451704563236867", Facebook: "", OldNames: [
+        "Zay"
+    ]},
+    {Name: "Monica", Discord: "384773585736171531", Facebook: "MonicaBrianna", OldNames: [
+        "Phact"
+    ]},
+    {Name: "Micah", Discord: "226532170477469697", Facebook: "", OldNames: [
+        "flareon", "lolabunnii", "derangeddisorder"
+    ]},
+    {Name: "Shayla", Discord: "140208760890064896", Facebook: "", OldNames: [
+        "darciblevins", "jolteon", "absurd"
+    ]},
+    {Name: "Paul", Discord: "125889066813095936", Facebook: "", OldNames: [
+        "enigma", "mortality"
+    ]},
+    {Name: "Viriseja", Discord: "365444374135439360", Facebook: "", OldNames: [
+        "Vaguba", "godfather"
+    ]},
+    {Name: "Becca", Discord: "155445844236042241", Facebook: "", OldNames: [
+        "Candikisses", "-snookums-"
+    ]},
+    {Name: "bluemansteele", Discord: "243636983392501760", Facebook: "", OldNames: [
+        "bluemansteele"
+    ]},
+    {Name: "Erin", Discord: "141232266461315072", Facebook: "hippiechickx", OldNames: [
+        "gangaaa", "laquandataylor"
+    ]},
+    {Name: "Karen", Discord: "466031428623269928", Facebook: "", OldNames: [
+        "devilhurt"
+    ]},
+    {Name: "Kyle", Discord: "138370326483566592", Facebook: "ultrafunkfantasy", OldNames: [
+        "5tevo"
+    ]},
+    {Name: "Halycen", Discord: "212274387821264896", Facebook: "", OldNames: [
+        "DJ_Future", "Past", "Future7565677"
+    ]},
+    {Name: "Brian", Discord: "581330484877852672", Facebook: "brian.gonyer", OldNames: [
+        "Slimey"
+    ]},
+    {Name: "Manda", Discord: "589953573492883466", Facebook: "", OldNames: [
+        "W0ahitzmanda"
+    ]},
+    {Name: "Matt", Discord: "142833773527040000", Facebook: "MatthewA412", OldNames: [
+        "darkriderz412", "vSideJesus"
+    ]},
+    {Name: "Raul", Discord: "333105900598591498", Facebook: "", OldNames: [
+        "-Panic"
+    ]},
+    {Name: "Beatrice", Discord: "", Facebook: "BEAH1111", OldNames: [
+        "gabsrockaway"
+    ]},
+    {Name: "Marissa", Discord: "", Facebook: "MarissaCharlottee", OldNames: [
+        "Mali78", "Mali_Boo", "xo_rissadoll_ox"
+    ]},
+    {Name: "Thomas", Discord: "265549293551616012", Facebook: "theflin1", OldNames: [
+        "Mali78", "Mali_Boo", "xo_rissadoll_ox"
+    ]},
+    {Name: "Margarethe", Discord: "", Facebook: "margarethe.unterberg", OldNames: [
+        "MargaretheU"
+    ]},
+    {Name: "Bryan", Discord: "179372664861884416", Facebook: "bryan.alcala", OldNames: [
+        "Bwyan"
+    ]},
+    {Name: "Joyzipie", Discord: "", Facebook: "Joyzipie", OldNames: [
+        "Crazyfilipina"
+    ]},
+    {Name: "Taylor", Discord: "", Facebook: "taylor.vakarian.3", OldNames: [
+        "omgitstaw"
+    ]},
+    {Name: "Jorge", Discord: "", Facebook: "jadorno16", OldNames: [
+        "jadorno"
+    ]},
+    {Name: "Hargest", Discord: "", Facebook: "hargefries", OldNames: [
+        "RuffDraft", "Friday", "J_Ruff"
+    ]},
+    {Name: "Savage", Discord: "", Facebook: "GlamSavage", OldNames: [
+        "JuanDallas"
+    ]},
+    {Name: "Amber", Discord: "", Facebook: "amzelindistress", OldNames: [
+        "RawrrItsAmberr", "Moonstruck"
+    ]},
+    {Name: "James", Discord: "", Facebook: "DeAdZeRoEfFeCt", OldNames: [
+        "DeAdZeRoEfFeCt"
+    ]},
+    {Name: "Dominyka", Discord: "", Facebook: "angelskayakukushka", OldNames: [
+        "Domiuks"
+    ]},
+    {Name: "Cheryl", Discord: "", Facebook: "cherylyong", OldNames: [
+        "Fuyuka"
+    ]},
+    {Name: "Kyra", Discord: "", Facebook: "kyrawr", OldNames: [
+        "Kyraaa", "tingle"
+    ]},
+    {Name: "Wyatt", Discord: "", Facebook: "WyattDavies", OldNames: [
+        "gamer125"
+    ]},
+    {Name: "Jessi", Discord: "", Facebook: "psychobabeh", OldNames: [
+        "ev0l", "oxl0vox", "screamingwhisper", "expired", "ilovehaydenb", "jesssi"
+    ]},
+    {Name: "Samantha", Discord: "", Facebook: "samantha.dzurkoc.3", OldNames: [
+        "dance2theflame"
+    ]},
+    {Name: "Jose", Discord: "", Facebook: "jesuriel", OldNames: [
+        "Jesuriel"
+    ]},
+    {Name: "Zuhaib", Discord: "", Facebook: "zeeferall", OldNames: [
+        "Zeeferall"
+    ]},
+    {Name: "Krissy", Discord: "", Facebook: "krissy.awesomeness", OldNames: [
+        "Bassguitar93", "pumpernickelyummy"
+    ]},
+    {Name: "Courtney", Discord: "", Facebook: "CourtneyRanallo", OldNames: [
+        "Courtneyanna"
+    ]},
+    {Name: "Mary", Discord: "", Facebook: "MaryNino89", OldNames: [
+        "MaryNino", "MaryN"
+    ]},
+    {Name: "Albert", Discord: "", Facebook: "askiLLs", OldNames: [
+        "Askills"
+    ]},
+    {Name: "Susan", Discord: "", Facebook: "suuusim", OldNames: [
+        "Miisu"
+    ]},
+    {Name: "Emily", Discord: "", Facebook: "Emily.J.Sloan", OldNames: [
+        "Limebear"
+    ]},
+    {Name: "Chris", Discord: "", Facebook: "darksideofthem0on", OldNames: [
+        "Cjay702", "-007"
+    ]},
+    {Name: "Laurie", Discord: "", Facebook: "laurie.hewitt.77", OldNames: [
+        "mayflower550"
+    ]},
+    {Name: "Morgan", Discord: "", Facebook: "morgan.lemieux", OldNames: [
+        "morgieeee"
+    ]},
+    {Name: "Morgan", Discord: "", Facebook: "morgan.moss6", OldNames: [
+        "morg6"
+    ]},
+    {Name: "Erica", Discord: "", Facebook: "omgawd.its.rica", OldNames: [
+        "Meow_MixXx"
+    ]},
+    {Name: "Ashley", Discord: "", Facebook: "ashleyxmac", OldNames: [
+        "ashley_mac", "ashleym"
+    ]},
+    {Name: "Melody", Discord: "", Facebook: "melodyb", OldNames: [
+        "Meiko"
+    ]},
+    {Name: "Andre", Discord: "", Facebook: "amazingandre", OldNames: [
+        "Scherzinger444", "AndreK"
+    ]},
+    {Name: "Francarlos", Discord: "", Facebook: "puntor", OldNames: [
+        "puntor"
+    ]},
+    {Name: "Jamie", Discord: "", Facebook: "JSquadd", OldNames: [
+        "ox--Shyla--ox"
+    ]},
+    {Name: "Karan", Discord: "133322894959837185", Facebook: "theskymightfalll", OldNames: [
+        "insanelyxparanoid"
+    ]},
+    {Name: "Holly", Discord: "", Facebook: "hollymay77", OldNames: [
+        "allaboutme77"
+    ]},
+    {Name: "Robert", Discord: "", Facebook: "", OldNames: [
+        "RLMJ93", "cataclysmic", "Metronome", "elbow", "lostsoul"
+    ]},
+    {Name: "Joshua", Discord: "", Facebook: "Mustang18m", OldNames: [
+        "Mustang_Killer"
+    ]},
+    {Name: "Gary-Joseph", Discord: "", Facebook: "Garyjoe2", OldNames: [
+        "GJ_2"
+    ]},
+    {Name: "Jovi", Discord: "", Facebook: "joviuy", OldNames: [
+        "Ivoj"
+    ]},
+    {Name: "Carina", Discord: "", Facebook: "Carry1992", OldNames: [
+        "Carry92", "CarryG"
+    ]},
+    {Name: "Vashtiana", Discord: "", Facebook: "vashtiana", OldNames: [
+        "Loud_mouth"
+    ]},
+    {Name: "Nikita", Discord: "", Facebook: "NiNiBuqq", OldNames: [
+        "Tanessa", "NikikaMarie"
+    ]},
+    {Name: "Ilan", Discord: "", Facebook: "ilan.kozirev", OldNames: [
+        "partyon132"
+    ]},
+    {Name: "Ashlee", Discord: "", Facebook: "ashiex1", OldNames: [
+        "Ashbombx32", "angieex3"
+    ]},
+    {Name: "Alex", Discord: "", Facebook: "earthtoalexxa", OldNames: [
+        "alexamay"
+    ]},
+    {Name: "Robert", Discord: "", Facebook: "iflytechy", OldNames: [
+        "DJSpinner", "-iRobby"
+    ]},
+    {Name: "Carlyn", Discord: "", Facebook: "CarlyBearx", OldNames: [
+        "XBetterOffAlonex", "CarlyRoxKisses"
+    ]},
+    {Name: "Laurens", Discord: "", Facebook: "lauwster", OldNames: [
+        "Lauw"
+    ]},
+    {Name: "Alyssa", Discord: "", Facebook: "Allyssaurus", OldNames: [
+        "Aly_saurus"
+    ]},
+    {Name: "Crystal", Discord: "", Facebook: "cmschupp", OldNames: [
+        "Groovenaut", "Groovenaut5"
+    ]},
+    {Name: "Lorenzo", Discord: "", Facebook: "LorenzoLabyrinth", OldNames: [
+        "Lnknprkfn"
+    ]},
+    {Name: "Carly", Discord: "", Facebook: "carly.fletcher1", OldNames: [
+        "angie8650"
+    ]},
+    {Name: "Katie", Discord: "", Facebook: "kfdake", OldNames: [
+        "maisye"
+    ]},
+    {Name: "Denise", Discord: "", Facebook: "denjaviermd", OldNames: [
+        "sweetangel21"
+    ]},
+    {Name: "Rachel", Discord: "", Facebook: "rledwards98", OldNames: [
+        "LuLubug"
+    ]},
+    {Name: "Kayla", Discord: "", Facebook: "KaylaMeganIhler", OldNames: [
+        "Munchkin_x"
+    ]},
+    {Name: "Cassandra", Discord: "", Facebook: "snowdazzlewaffle", OldNames: [
+        "Snowdazzlewaffle"
+    ]},
+    {Name: "Dennis", Discord: "", Facebook: "deward", OldNames: [
+        "Dew7", "Dewurite7"
+    ]},
+    {Name: "Ameil", Discord: "", Facebook: "AmeilFourshee", OldNames: [
+        "Complex", "iPhantom"
+    ]},
+    {Name: "Katrina", Discord: "", Facebook: "kjgibbers", OldNames: [
+        "Katrinaa", "Katapult"
+    ]},
+    {Name: "Jordan", Discord: "", Facebook: "jordanteifert", OldNames: [
+        "Aztam"
+    ]},
+    {Name: "Susana", Discord: "", Facebook: "susana.cobian", OldNames: [
+        "Sassy19"
+    ]},
+    {Name: "Kristin", Discord: "", Facebook: "KristinSkyline", OldNames: [
+        "krisanka"
+    ]},
+    {Name: "Kelsey", Discord: "", Facebook: "Kelsey.Kels.5683", OldNames: [
+        "Angel62866"
+    ]},
+    {Name: "Hans", Discord: "", Facebook: "H.v.Hagen", OldNames: [
+        "Valentios"
+    ]},
+    {Name: "Drew", Discord: "", Facebook: "drewbilee.drew.7", OldNames: [
+        "Terkoiiz"
+    ]},
+    {Name: "Hope", Discord: "", Facebook: "profile.php?id=100008163404729", OldNames: [
+        "ZombieLand"
+    ]},
+    {Name: "Pinelopi", Discord: "", Facebook: "pinelopiTUMBAO", OldNames: [
+        "Pitsapo", "iwishmjwashere", "Michael Jackson's sunflower"
+    ]},
+    {Name: "Dalton", Discord: "", Facebook: "profile.php?id=100008956773781", OldNames: [
+        "Imagical"
+    ]},
+    {Name: "Robert", Discord: "", Facebook: "mucski", OldNames: [
+        "xRob", "Kasumi", "mucski"
+    ]},
+    {Name: "Deonta", Discord: "", Facebook: "deonta.holloway.7", OldNames: [
+        "DJ_Realistic"
+    ]},
+    {Name: "Norazizah", Discord: "", Facebook: "jijsapphire", OldNames: [
+        "_A_L_E_X_I_S_"
+    ]},
+    {Name: "Lo", Discord: "", Facebook: "xoloveme.envymeox", OldNames: [
+        "FloeticVibez23"
+    ]},
+    {Name: "Justin", Discord: "", Facebook: "justin.ernest.clement", OldNames: [
+        "justinforasec"
+    ]},
+    {Name: "Florence", Discord: "", Facebook: "fmpebs", OldNames: [
+        "florencemae"
+    ]},
+    {Name: "Sigal", Discord: "", Facebook: "sigal.meow", OldNames: [
+        "Diana9535"
+    ]},
+    {Name: "Nazimovaitė", Discord: "", Facebook: "profile.php?id=100015654070746", OldNames: [
+        "Amaya"
+    ]},
+    {Name: "Dilip", Discord: "", Facebook: "EnergizeX", OldNames: [
+        "vAdam"
+    ]},
+    {Name: "Maurcus", Discord: "", Facebook: "maurcus", OldNames: [
+        "Kertious", "MarkD-"
+    ]},
+    {Name: "Chakinah", Discord: "", Facebook: "chakinah", OldNames: [
+        "iGlaMDollx"
+    ]},
+    {Name: "Karina", Discord: "", Facebook: "Karina.Tomaseviciute5", OldNames: [
+        "Reiiia"
+    ]},
+    {Name: "Brandon", Discord: "", Facebook: "MrDerryberry", OldNames: [
+        "Dance_master"
+    ]},
+    {Name: "Cherie", Discord: "", Facebook: "CherrySc33", OldNames: [
+        "CherrySwirl33"
+    ]},
+    {Name: "Cory", Discord: "", Facebook: "", OldNames: [
+        "Gayboi29", "CrunchyOaks"
+    ]},
+    {Name: "Rose", Discord: "", Facebook: "78roses", OldNames: [
+        "redrose28"
+    ]},
+]
+
+const options = {
+    caseSensitive: false,
+    excludedKeys: ['Discord', 'Facebook'],
+}
+
+const customFind = (collection, term, opts) => {
+    const filterBy = () => {
+        const searchTerms = (!opts.caseSensitive) ? new RegExp(term, 'i') : new RegExp(term)
+        return (obj) => {
+            for (const key of Object.keys(obj)) {
+                if (searchTerms.test(obj[key]) &&
+                !opts.excludedKeys.includes(key)) return true
+            }
+            return false;
+        }
+    }
+    return collection.filter(filterBy(term))
+}
+
+function compare(a, b){
+    a = a.Name.toLowerCase();
+    b = b.Name.toLowerCase();
+    if (a > b) return 1;
+    if (b > a) return -1;
+    return 0;
+}
+
+
+var preVal = "";
+$( "#usersearch" ).keyup(function() {
+    var searchVal = $("#usersearch").val()
+    if ( searchVal === preVal) {
+        return;
+    } else if (searchVal == "") {
+        $("#results").html("");
+        return;
+    } else if (searchVal.length < 2) {
+        $("#results").html("");
+        return;
+    }
+    preVal = searchVal;
+    const found = customFind(users, searchVal, options);
+    found.sort(compare);
+    var res = "";
+
+    found.forEach(user => {
+        console.log('Found')
+        res += `<div class="card card-sm m-3 d-flex d-flex-row" style="flex-direction: row;">
+                <div class="p-2 flex-grow-1">
+                    <h3>${user.Name}</h3>`;
+                    for (i=0; i< user.OldNames.length; i++) {
+                        if (user.OldNames[i].toLowerCase().includes(searchVal.toLowerCase())) {
+                            res += `<a href="#" class="badge badge-info">${user.OldNames[i]}</a>`;
+                        } else {
+                            //res += `<a href="#" class="badge badge-dark">${user.OldNames[i]}</a>`;
+                        }
+                    };
+                res += `</div>`;
+                res += `<div class="p-2 flex-shrink-1 social">`;
+                    if (user.Discord !== "") {
+                        res += `<a href="https://discordapp.com/users/${user.Discord}" rel="nofollow" target="_blank" title="Open profile in discord (Only shows if you're in the server)"><i class="fab fa-discord"></i></a>`;
+                    }
+                    if (user.Facebook !== "") {
+                        res += `<a href="https://www.facebook.com/${user.Facebook}" rel="nofollow" target="_blank" title="Open Facebook profile"><i class="fab fa-facebook-square"></i></a>`;
+                    }
+                res += `</div></div>`;
+
+    });
+
+    if (res === "") {
+        res = "No vSiders Found"
+    }
+
+    $("#results").html(res);
+    console.log(found);
+});
